7ad803f98ae63d272ebc41c9183402711348c8dc
YJE <c0d1ngyje@gmail.com>
2025-09-01 17:54:05 +0900

    feat: DTO 검증 데코레이터 추가

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,9 +1,11 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateTodoDto {
-  @IsString()
+  @IsString({ message: '제목은 문자열만 가능합니다.' })
+  @MinLength(3, { message: '제목은 $constraint1자 이상 작성해주세요.' })
+  @MaxLength(30, { message: '제목은 $constraint1자를 넘길 수 없습니다.' })
   title: string;
 
-  @IsBoolean()
+  @IsBoolean({ message: 'Boolean 타입만 가능합니다.' })
   isDone: boolean;
 }
